3d355a2fd2262ca621a136254e984fa6884beb35
Tai Tran <tai.tran@mgisolutions.com>
2023-06-15 18:01:50 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 console.log(import.meta.env.VITE_SOME_KEY);
 console.log(import.meta.env.BASE_URL);
